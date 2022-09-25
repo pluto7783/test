dfe02a438ceb9a6eb71c51b9b95c309b06702779
karolus <karolus@naver.com>
2022-09-25 20:57:22 +0900

    test2

--- src/main/java/com/example/test/servlet/frontcontroller/v2/FrontControllerServletV2.java
+++ src/main/java/com/example/test/servlet/frontcontroller/v2/FrontControllerServletV2.java
@@ -28,7 +28,7 @@ public class FrontControllerServletV2 extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("FrontControllerServletV2.service");
+        System.out.println("FrontControllerServletV2.service2");
 
         String requestURI = request.getRequestURI();
 
